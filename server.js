e5f79f0881ae383ab0c794c2125c4668a62a2109
JayHChoi <jaychoi1619@gmail.com>
2019-01-28 23:44:34 +0900

    Initial commit & Add setup express server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { MONGODB_URL } = require('./config/keys');
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+const app = express();
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI || MONGODB_URL,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('hello!'));
+
+// USER ROUTES
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
