7d87f1a93bacb3e3f925e95f04839d9c154f414b
JayHChoi <jaychoi1619@gmail.com>
2019-02-12 12:59:20 +0900

    Setup to deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const { MONGODB_URL } = require('./config/keys');
+const { MONGODB_URI } = require('./config/keys');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
@@ -16,10 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false })); // This is for easy request
 app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    process.env.MONGODB_URI || MONGODB_URL,
-    { useNewUrlParser: true }
-  )
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
@@ -30,6 +27,18 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
+
+// Production behaviors
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up static assets
+  app.use(express.static('client/build'));
+
+  // Express responses with root html file
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
